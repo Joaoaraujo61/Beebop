e9fd55ff3b8be14918a1ad5ffd220a5db7ca9815
Joaoaraujo61 <joaopedrodearaujo61@gmail.com>
2026-08-20 21:21:19 -0300

    header finalizado

--- js/components/header.js
+++ js/components/header.js
@@ -5,27 +5,33 @@ export function renderHeader({ onSearch, initialQuery = '' } = {}) {
   header.className = 'header';
 
   header.innerHTML = `
-    <div class="header__logo">
-      <a href="/index.html"><img src="../../../assets/beebop.png"></a>
-   
-    <form class="header__search-form">
-      <input 
-        type="text" 
-        class="header__search-input" 
-        placeholder="Buscar músicas, álbuns, artistas..." 
-        value="${initialQuery}"
-      />
-      <button type="submit" class="header__search-btn">Buscar</button>
-    </form>
-    <nav class="header__nav">
-      <a href="/playlists.html">Playlists</a>
-    </nav> </div>
+      <div class="header__logo_nav">
+        <a href="/index.html"><img src="../../../assets/beebop.png"></a>
+        <nav class="header__nav">
+          <a href="/playlists.html">Inicio</a>
+          <a href="/playlists.html">Explorar</a>
+          <a href="/playlists.html">Charts</a>
+        </nav>
+      </div>
+      <form class="header__search_form">
+        <input
+          type="text"
+          class="header__search_input"
+          placeholder="Buscar músicas, álbuns, artistas..."
+          value="${initialQuery}"
+        />
+      <i class="fa-solid fa-magnifying-glass" style="color: rgb(156, 163, 175);"></i>
+      </form>
+      <div class="header_login">
+        <a href="/playlists.html">Entrar</a>
+        <a href="/playlists.html" class="header_account">Criar Conta</a>
+      </div>
   `;
 
   // Eventos ficam encapsulados aqui dentro — a página não precisa
   // saber COMO o header funciona, só o QUE ele faz (callback)
-  const form = header.querySelector('.header__search-form');
-  const input = header.querySelector('.header__search-input');
+  const form = header.querySelector('.header__search_form');
+  const input = header.querySelector('.header__search_input');
 
   form.addEventListener('submit', (event) => {
     event.preventDefault();
